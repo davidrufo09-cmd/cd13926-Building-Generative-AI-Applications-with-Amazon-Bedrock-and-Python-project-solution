6fd48ac1d35c66af0d78afeb1703f2f2cbcef512
davidrufo09-cmd <davidrufo09@gmail.com>
2026-05-03 17:50:14 +0000

    Update configuration for us-west-2 deployment

--- stack2/main.tf
+++ stack2/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-west-2"  
+  region = "us-west-2"
 }
 
 module "bedrock_kb" {
@@ -8,15 +8,15 @@ module "bedrock_kb" {
   knowledge_base_name        = "my-bedrock-kb"
   knowledge_base_description = "Knowledge base connected to Aurora Serverless database"
 
-  aurora_arn        = "" #TODO Update with output from stack1
+  aurora_arn      = "arn:aws:rds:us-west-2:594695117986:cluster:my-aurora-serverless"
   aurora_db_name    = "myapp"
-  aurora_endpoint   = "" # TODO Update with output from stack1
+  aurora_endpoint  = "my-aurora-serverless.cluster-c2pch4muptyq.us-west-2.rds.amazonaws.com"
   aurora_table_name = "bedrock_integration.bedrock_kb"
   aurora_primary_key_field = "id"
   aurora_metadata_field = "metadata"
   aurora_text_field = "chunks"
   aurora_verctor_field = "embedding"
   aurora_username   = "dbadmin"
-  aurora_secret_arn = "" #TODO Update with output from stack1
-  s3_bucket_arn = "" #TODO Update with output from stack1
+  aurora_secret_arn = "arn:aws:secretsmanager:us-west-2:594695117986:secret:my-aurora-serverless-UK4Eua"
+  s3_bucket_arn = "arn:aws:s3:::bedrock-kb-594695117986"
 }
